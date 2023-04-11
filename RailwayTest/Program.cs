30ceb68ecab03c17e06489234d1e9645ea17e8c2
李仁兵 <153662435@qq.com>
2023-04-11 10:56:42 +0800

    提交修改

--- RailwayTest/Program.cs
+++ RailwayTest/Program.cs
@@ -14,8 +14,13 @@ builder.Services.AddSwaggerGen(c =>
 
 var app = builder.Build();
 // Configure the HTTP request pipeline.
-app.UseSwagger();
-app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1"); });
+var enableSwagger = Environment.GetEnvironmentVariable("ENABLE_SWAGGER");
+if (!string.IsNullOrEmpty(enableSwagger) && bool.Parse(enableSwagger))
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1"); });
+}
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
