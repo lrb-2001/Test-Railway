b20eadab3b8a55d6164e38382ad28b4832d74f44
李仁兵 <153662435@qq.com>
2023-04-14 09:39:08 +0800

    提交修改

--- RailwayTest/Program.cs
+++ RailwayTest/Program.cs
@@ -1,18 +1,20 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+//var PORT = Environment.GetEnvironmentVariable("$PORT");
+//builder.WebHost.UseUrls("http://[*]:"+PORT);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+var enableSwagger = Environment.GetEnvironmentVariable("ENABLE_SWAGGER");
+if (!string.IsNullOrEmpty(enableSwagger) && bool.Parse(enableSwagger))
 {
-    
 }
 app.UseSwagger();
 app.UseSwaggerUI();
